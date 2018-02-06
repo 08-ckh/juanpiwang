15f8581d20d02104a7d1ad9112bbd2e3fa30aa95
cao <1099468902@qq.com>
2018-02-06 21:37:18 +0800

    飞入购物车+静态小优化

---
+++ src/js/details.flyingCar.js
@@ -0,0 +1,39 @@
+(function(){
+	let totalBtn1 = 0,
+		totalBtn2 = 0;
+	function clickBtn() {
+		$('#button').on('click',function () {
+			let img=$('#img1');
+			console.log(img);
+			let flyImg = img.clone().css({
+				'opacity':'0.6'
+			});
+			$('.main').append(flyImg);
+			flyImg.css({
+				'z-index':999,
+				'border':'3px solid #fff',
+				'position': 'absolute',
+				'height' : img.height() + 'px',
+				'width' : img.width() + 'px',
+				'top' : img.offset().top +'px',
+				'left' : img.offset().left + 'px'
+			})
+			flyImg.animate({
+				'width' : 50 + 'px',
+				'height' : 50 + 'px',
+				'border-radius' : 100 + '%'
+			},function(){
+				let	t = $('#btn1-add').offset().top+20;
+				flyImg.animate({
+					'top':t,
+					'left':($(document).width()-$('.car').width()) + 'px',
+					'height':0 +'px',
+					'width' :0+'px',
+				},1500,function(){
+					flyImg.remove();
+				})
+			});
+		})
+	}
+	clickBtn();
+})();
